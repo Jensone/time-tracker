0440e65dc80eed797c69a305a2ac28305495c626
Jensone <jeancharlesjensone@gmail.com>
2022-10-02 16:57:13 +0200

    Filter settings added

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Filter stetting
+let filterSelected
+document.querySelector("#Daily").onclick = function () { filterSelected = "Daily" }
+document.querySelector("#Weekly").onclick = function () { filterSelected = "Weekly" }
+document.querySelector("#Monthly").onclick = function () { filterSelected = "Monthly" }
 
 // Fetch my json file
 fetch('data.json')
@@ -5,10 +10,13 @@ fetch('data.json')
     .then(data => {
         // Select the ul tag to display the data
         let activities = document.querySelector("#activities")
+
         // Display my data in a loop with a li tag
         for (let i of data) { 
             let item = document.createElement("article")
-            item.innerHTML = 
+            // Filter the data
+            if(filterSelected == "Monthly"){
+                item.innerHTML = 
             `
             <section class="${i.title} flex flex-column flex-justify-center">
                 <header class="flex flex-justify-between w100">
@@ -18,23 +26,54 @@ fetch('data.json')
                     </a>
                 </header>
                 <main class="flex flex-align-center flex-justify-between w100">
-                    <p class="hours">32hrs</p>
-                    <p class="past-hours">Last Week - <span >36hrs</span></p>
+                    <p class="hours">${i.timeframes.monthly.current}hrs</p>
+                    <p class="past-hours">Last Week - <span >${i.timeframes.monthly.previous}hrs</span></p>
                 </main>
             </section>
             `
-            item.classList.add("activity");
-            // item.classList.add(i.title);
+            }
+            else if(filterSelected == "Weekly"){
+                item.innerHTML = 
+            `
+            <section class="${i.title} flex flex-column flex-justify-center">
+                <header class="flex flex-justify-between w100">
+                    <h4>${i.title}</h4>
+                    <a href="#">
+                        <svg width="32" height="32" viewBox="0 0 16 16"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><circle cx="2.5" cy="8" r=".75"/><circle cx="8" cy="8" r=".75"/><circle cx="13.5" cy="8" r=".75"/></g></svg>
+                    </a>
+                </header>
+                <main class="flex flex-align-center flex-justify-between w100">
+                    <p class="hours">${i.timeframes.weekly.current}hrs</p>
+                    <p class="past-hours">Last Week - <span >${i.timeframes.weekly.previous}hrs</span></p>
+                </main>
+            </section>
+            `
+            }
+            else {
+                item.innerHTML = 
+            `
+            <section class="${i.title} flex flex-column flex-justify-center">
+                <header class="flex flex-justify-between w100">
+                    <h4>${i.title}</h4>
+                    <a href="#">
+                        <svg width="32" height="32" viewBox="0 0 16 16"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><circle cx="2.5" cy="8" r=".75"/><circle cx="8" cy="8" r=".75"/><circle cx="13.5" cy="8" r=".75"/></g></svg>
+                    </a>
+                </header>
+                <main class="flex flex-align-center flex-justify-between w100">
+                    <p class="hours">${i.timeframes.daily.current}hrs</p>
+                    <p class="past-hours">Last Week - <span >${i.timeframes.daily.previous}hrs</span></p>
+                </main>
+            </section>
+            `
+            }
+            item.classList.add("activity")
             activities.appendChild(item)
         }
-        document.querySelector("#activities").appendChild(activity);
-        // data.forEach(data => {
-	    // activity.createElement('Nom: ', data.title);
-        // console.log("C'est passé");
+        document.querySelector("#activities").appendChild(activity)
+        console.log("Seems all it's ok 😍")
 	})
-   // return data;
     .catch(error => {
-        console.log("I dunno 🤷");
+        console.log("I dunno what to display 🤷")
     })
 
 
